bab87332845a91cd4f6533b307b74eb240d0151a
1004712595 <1004712595@qq.com>
2016-09-15 16:12:40 +0800

    Initial commit

---
+++ mytest/mytest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mytest
+//
+//  Created by 颜欢 on 16/9/15.
+//  Copyright © 2016年 颜欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mytest/mytest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mytest
+//
+//  Created by 颜欢 on 16/9/15.
+//  Copyright © 2016年 颜欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
