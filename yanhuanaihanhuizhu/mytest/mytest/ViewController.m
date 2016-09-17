2bb3b4880d1370613475dbf5a6d591c98b2b0431
1004712595 <1004712595@qq.com>
2016-09-17 15:49:20 +0800

    New feature complete.

---
+++ yanhuanaihanhuizhu/mytest/mytest/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  mytest
+//
+//  Created by 颜欢 on 16/9/15.
+//  Copyright © 2016年 颜欢. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"我爱韩蕙竹一生一世！");
+    NSLog(@"小竹竹你个淘气的小坏蛋，你家欢大官人要打你的小屁屁");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
